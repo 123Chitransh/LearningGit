3761ae92c7db95a43b00038629691d8e9b8f82fd
Chitransh Soni <chitransh7017soni@gmail.com>
2024-07-20 13:18:04 +0530

    second updation is done

--- 2sum.cpp
+++ 2sum.cpp
@@ -2,5 +2,7 @@
 using namespace std;
 int main(){
     string name = "Chinu Soni";
+    string secondname = "Chitransh Soni";
     cout<<"My name is "<<name<<endl;
+    cout<<"my second name is "<<secondname<<endl;
 }
